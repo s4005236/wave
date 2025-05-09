62c69f9ddeae37ef274bdfb8ae2276b613a777b8
Alexander Jahn <ajahn@ibes.ag>
2025-05-09 09:18:54 +0200

    feat: add setup script to install dependencies and pre-commit hooks

---
+++ setup.py
@@ -0,0 +1,41 @@
+"""
+This script sets up the project by
+installing dependencies and setting up pre-commit hooks.
+"""
+
+import subprocess
+import sys
+
+
+def run(cmd: str):
+    """
+    Run a shell command and exit if it fails.
+
+    Args
+        - cmd (str): The command to run.
+
+    """
+    print(f"→ {cmd}")
+    result = subprocess.run(cmd, shell=True, check=False)
+    if result.returncode != 0:
+        sys.exit(result.returncode)
+
+
+def main():
+    """
+    Defines the commands to setup the project.
+    workload:
+        - Upgrade pip
+        - Install pip and poetry
+        - Install dependencies using poetry
+        - Install pre-commit hooks
+    """
+    print("Setting up the project...")
+    run("pip install --upgrade pip")
+    run("pip install poetry")
+    run("poetry update")
+    run("pre-commit install")
+
+
+if __name__ == "__main__":
+    main()
