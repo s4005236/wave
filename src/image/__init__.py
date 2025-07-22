8135a9461654e1cecb44ed9a63cd1e7e38c17453
Kowarsch, Jaden Dustin <Kowarsch.Jaden-Dustin@fes-aes.de>
2025-07-22 11:30:48 +0200

    feat: Mediapipe integration

--- src/image/__init__.py
+++ src/image/__init__.py
@@ -1,32 +1,65 @@
 import cv2 as cv
+import mediapipe as mp
 import numpy as np
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.vision import HandLandmarker
+from mediapipe.tasks.python.vision.hand_landmarker import HandLandmarkerOptions
 
 
-def start_camera():
-    cap = cv.VideoCapture(0)
+def initialize_mediapipe_hands(model_path: str):
+    """
+    Defines the different settings to mark the different parts of the hands
+    """
+    options = HandLandmarkerOptions(
+        base_options=mp.tasks.BaseOptions(model_asset_path=model_path),
+        running_mode=vision.RunningMode.VIDEO,
+        num_hands=2,
+    )
+    hand_landmarker = HandLandmarker.create_from_options(options)
+    return hand_landmarker
 
-    if not cap.isOpened():
-        print("Cannot open camera")
-        exit()
-    while True:
+
+def allknowing_function():
+    """
+    Knows and does everything!
+    """
+    model_path = "build\\lib\\mediapipe\\hand_landmarker.task"
+    hand_landmarker = initialize_mediapipe_hands(model_path)
+
+    cap = cv.VideoCapture(1)
+    timestamp = 0
+
+    while cap.isOpened():
         ret, frame = cap.read()
-
         if not ret:
-            print("Cannot receive frame. Exiting...")
+            break
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+        results = hand_landmarker.detect_for_video(mp_image, timestamp)
+
+        if results.hand_landmarks:
+            for hand_landmarks in results.hand_landmarks:
+                for landmark in hand_landmarks:
+                    x_px = int(landmark.x * frame.shape[1])
+                    y_px = int(landmark.y * frame.shape[0])
+                    cv.circle(frame, (x_px, y_px), 5, (0, 255, 0), -1)
+
+        cv.imshow("WAVE Hand Tracking", frame)
+        if cv.waitKey(1) & 0xFF == ord("q"):
             break
 
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        cv.imshow("WAVE Camera Module", gray)
-
-        if cv.waitKey(1) == ord("q"):
-            break
+        timestamp += int(1000 / cap.get(cv.CAP_PROP_FPS))
 
     cap.release()
-    cv.destroyAllWindows()
+    cv.destroyAllWindows
 
 
 def main():
-    start_camera()
+    """
+    Executes the functions in the right order
+    """
+    allknowing_function()
 
 
 if __name__ == "__main__":
