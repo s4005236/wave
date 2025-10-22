dbe0eeb9d4b71832b665be60a862ea7babbb166c
Svcks <m28unger@gmail.com>
2025-10-22 14:03:15 +0200

    restoring the latest stable version before changes were made

--- wave/image/__init__.py
+++ wave/image/__init__.py
@@ -3,24 +3,34 @@ import time
 import cv2
 from picamera2 import Picamera2
 
-picam = Picamera2()
-# size doesn't have to be in there, at least it's not working for my setup when I change the values
-# default is 640 x 480
-config = picam.configure(
-    picam.create_preview_configuration(main={"size": (640, 480)})
-)
+# Configuration
+FPS = 10
+
+picam = (
+    Picamera2()
+)  # is needed to use Raspberry Pi Cameras that are connected via CSI Interface
+picam.configure(picam.create_preview_configuration(main={"size": (640, 480)}))
 picam.start()
 time.sleep(1)
 
+frame_time = 1.0 / FPS  # time between two frames
+
 while True:
+    start = time.time()
     frame = picam.capture_array()
-    gray = cv2.cvtColor(
-        frame, cv2.COLOR_BGR2GRAY
-    )  # Grayscale, 0–255 for every pixel
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("wave - testing grayscale", gray)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    if cv2.waitKey(1) & 0xFF == ord(
+        "q"
+    ):  # returns ASCII-Code of the given Key; 0xFF ensures that only the last 8 bits of the actual key are being used
         break
-# test
+
+    elapsed = time.time() - start
+    delay = max(
+        0, frame_time - elapsed
+    )  # ensures positive numbers in case the loop needs more time than the fps cap
+    time.sleep(delay)  # "dynamic" delay based on the runtime of the loop
+
 picam.close()
 cv2.destroyAllWindows()
