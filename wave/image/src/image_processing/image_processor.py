0778f75330ca6f0dffc5417ee0f2eed6364dc9dd
ajahn <ajahn@ibes.ag>
2025-10-29 14:42:37 +0100

    feat: initializes file structure to make image processor accessible for REST API

---
+++ wave/image/src/image_processing/image_processor.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+log = logging.getLogger(__name__)
+
+
+class ImageProcessor:
+    """
+    The image processor module of this project.
+    Handles image processing tasks.
+    """
+
+    def __init__(self):
+        pass
+
+    def process(self):
+        """Main image processing function."""
+
+        # TODO put code for image processing here
+        pass
---
+++ wave/image/src/stores/gesture_store.py
@@ -0,0 +1,11 @@
+log = logging.getLogger(__name__)
+
+
+class GestureStore:
+    def __init__(self):
+        self.gestures = []
+
+    def send_detected_gesture(self):
+        # send detected gesture to the core
+
+        return self.gestures
