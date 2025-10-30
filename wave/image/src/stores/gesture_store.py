9016b4567bdad23bfcbf1c71678894784f55eb90
ajahn <ajahn@ibes.ag>
2025-10-30 14:49:56 +0100

    feat: implement gesture_store entity

--- wave/image/src/stores/gesture_store.py
+++ wave/image/src/stores/gesture_store.py
@@ -1,11 +1,43 @@
-log = logging.getLogger(__name__)
+from wave.models.dataclasses.gesture import Gesture
 
 
 class GestureStore:
+    """
+    Store for gestures received from the core module.
+    """
+
     def __init__(self):
+        self.gestures: list[Gesture] = []
+
+    def store_gestures(self, gesture_list: list[Gesture]) -> None:
+        """
+        Takes a list of gestures and stores them in the store.
+        """
+        self.gestures.append(gesture_list)
+        print(f"Successfully stored {len(self.gestures)} gesture(s).")
+
+    def get_gestures(self) -> list[Gesture] | None:
+        """
+        Retrieves a gesture by its id. If no gestures are stored, returns None.
+        """
+        if len(self.gestures) > 0:
+            return self.gestures
+        return None
+
+    def get_gesture_by_id(self, gesture_id: str) -> Gesture | None:
+        """
+        Retrieves a gesture by its id.
+        """
+        # TODO: Implement retrieval logic
+        print(f"get gesture with id {gesture_id}, not yet implemented.")
+        return None
+
+    def clear_gestures(self) -> None:
+        """
+        Clears all stored gestures.
+        """
         self.gestures = []
+        print("Successfully cleaned all stored gestures.")
 
-    def send_detected_gesture(self):
-        # send detected gesture to the core
 
-        return self.gestures
+gesture_store = GestureStore()
