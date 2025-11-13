5292e5ee240e289af9f1d29722cf158cf7b038e2
ajahn <ajahn@ibes.ag>
2025-11-13 10:34:18 +0100

    feat: define device model

---
+++ wave/models/dataclasses/device.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass  # , field
+
+# from uuid import UUID, uuid4
+
+
+@dataclass
+class Event:
+    """A base class representing a gesture."""
+
+    id: str
+    name: str
+    device_id: str
+    aspect: dict[str, list[str]]  # aspect and list of valid modifiers
+    # TODO uuid: UUID = field(default_factory=uuid4)
