d07cf700d09b19ea0c116a582aae6fe5a3eab676
Dustin Kowarsch <s4005362@edu.dhsn.de>
2025-11-25 10:43:48 +0100

    fix: Addes libcap dev dependecies

--- scripts/apt_installs.sh
+++ scripts/apt_installs.sh
@@ -3,5 +3,7 @@
 sudo apt update
 sudo apt full-upgrade -y
 sudo apt install -y python3-pip python3-venv build-essential git
+# Needed for working with Raspberry Pi 4
+sudo apt install -y python3-libcamera python3-kms++ libcap-dev
 
 # TODO setup all apt dependencies here
